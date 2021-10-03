1eccc124129a462a8e241a1412bac73a78b8ee6f
hsimao <e087754958@gmail.com>
2021-10-03 15:13:14 +0800

    feat: Uploader show file list logic

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="uploader">
-    <button @click="triggerUpload">
-      {{ buttonText }}
+    <button
+      class="uploader__button"
+      type="primary"
+      @click="triggerUpload"
+      :disabled="isUploading"
+    >
+      <LoadingOutlined v-if="isUploading" />
+      <template v-else>
+        上傳
+      </template>
     </button>
     <input
       v-show="false"
@@ -10,16 +18,50 @@
       type="file"
       @change="handleFileChange"
     />
+    <ul class="uploader__items">
+      <li
+        v-for="file in uploadedFiles"
+        :key="file.uid"
+        :class="`uploader__item uploader__item--${file.status}`"
+      >
+        <FileOutlined />
+        <span class="uploader__filename">{{ file.name }}</span>
+
+        <DeleteOutlined
+          class="uploader__delete"
+          @click="handleRemoveFile(file.uid)"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref, reactive } from 'vue';
+import {
+  DeleteOutlined,
+  LoadingOutlined,
+  FileOutlined
+} from '@ant-design/icons-vue';
+import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
+
 type UploadStatus = 'ready' | 'loading' | 'success' | 'error';
+export interface UploadFile {
+  uid: string;
+  size: number;
+  name: string;
+  status: UploadStatus;
+  raw: File;
+}
 
 export default defineComponent({
   name: 'Uploader',
+  components: {
+    DeleteOutlined,
+    LoadingOutlined,
+    FileOutlined
+  },
   props: {
     url: {
       type: String,
@@ -29,19 +71,11 @@ export default defineComponent({
   setup(props) {
     const fileInput = ref<null | HTMLInputElement>(null);
     const fileStatus = ref<UploadStatus>('ready');
+    const uploadedFiles = ref<UploadFile[]>([]);
 
-    const buttonText = computed(() => {
-      switch (fileStatus.value) {
-        case 'loading':
-          return '上傳中...';
-        case 'success':
-          return '上傳成功';
-        case 'error':
-          return '上傳失敗';
-        default:
-          return '上傳';
-      }
-    });
+    const isUploading = computed(() =>
+      uploadedFiles.value.some(file => file.status === 'loading')
+    );
 
     const triggerUpload = () => {
       if (fileInput.value) {
@@ -55,6 +89,16 @@ export default defineComponent({
       if (files) {
         const uploadedFile = files[0];
         const formData = new FormData();
+
+        const fileObj = reactive<UploadFile>({
+          uid: uuidv4(),
+          size: uploadedFile.size,
+          name: uploadedFile.name,
+          status: 'loading',
+          raw: uploadedFile
+        });
+        uploadedFiles.value.push(fileObj);
+
         formData.append(uploadedFile.name, uploadedFile);
 
         fileStatus.value = 'loading';
@@ -65,20 +109,34 @@ export default defineComponent({
             }
           })
           .then(res => {
-            console.log(res.data);
+            console.log(res);
             fileStatus.value = 'success';
+            fileObj.status = 'success';
           })
           .catch(() => {
             fileStatus.value = 'error';
+            fileObj.status = 'error';
+          })
+          .finally(() => {
+            // NOTE: reset input, 避免上傳兩張相同圖片, 不會觸發 change 問題
+            if (fileInput.value) {
+              fileInput.value.value = '';
+            }
           });
       }
     };
 
+    const handleRemoveFile = (id: string) => {
+      uploadedFiles.value = uploadedFiles.value.filter(file => file.uid !== id);
+    };
+
     return {
-      buttonText,
+      uploadedFiles,
+      isUploading,
       fileInput,
       triggerUpload,
-      handleFileChange
+      handleFileChange,
+      handleRemoveFile
     };
   }
 });
@@ -86,5 +144,56 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .uploader {
+  display: flex;
+  align-self: center;
+  flex-flow: column nowrap;
+  justify-content: center;
+  padding: 16px;
+
+  &__button {
+    padding: 3px;
+    max-width: 100px;
+    border: none;
+    border-radius: 3px;
+    background-color: #3f9eff;
+    color: white;
+    cursor: pointer;
+  }
+
+  &__items {
+    margin: 6px 0 0 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  &__filename {
+    margin-left: 6px;
+  }
+
+  &__item {
+    display: flex;
+    align-items: center;
+
+    & + & {
+      margin-top: 6px;
+    }
+
+    &--loading {
+      color: gray;
+    }
+
+    &--success {
+      color: #3f9eff;
+    }
+
+    &--error {
+      color: #f56c6b;
+    }
+  }
+
+  &__delete {
+    margin-left: auto;
+    cursor: pointer;
+  }
 }
 </style>
--- tests/unit/Uploader.spec.ts
+++ tests/unit/Uploader.spec.ts
@@ -8,11 +8,25 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 let wrapper: VueWrapper<any>;
 const testFile = new File(['xyz'], 'test.png', { type: 'image/png' });
 
+const mockComponent = {
+  template: '<span><svg></svg></span>'
+};
+
+const iconComponents = {
+  DeleteOutlined: mockComponent,
+  LoadingOutlined: mockComponent,
+  FileOutlined: mockComponent
+};
+
 describe('Uploader Component', () => {
   beforeAll(() => {
     wrapper = shallowMount(Uploader, {
       props: {
         url: 'https://httpbin.org/post'
+      },
+      global: {
+        // 組件內註冊的組件使用 stubs
+        stubs: iconComponents
       }
     });
   });
@@ -27,7 +41,7 @@ describe('Uploader Component', () => {
     expect(wrapper.get('.uploader__input').isVisible()).toBeFalsy();
   });
 
-  it('upload process should works find', async () => {
+  it('upload process should works find', done => {
     mockedAxios.post.mockResolvedValueOnce({ status: 'success' });
     // 模擬檔案上傳
     // 將 testFile 傳到 input 內
@@ -42,20 +56,53 @@ describe('Uploader Component', () => {
     wrapper.get('input').trigger('change');
     wrapper.vm.$nextTick(async () => {
       expect(mockedAxios.post).toHaveBeenCalledTimes(1);
-      expect(wrapper.get('button').text()).toBe('上傳中...');
+
+      // loading svg
+      expect(
+        wrapper
+          .get('button')
+          .find('svg')
+          .exists()
+      ).toBeTruthy();
+
+      // button disabled
+      expect(wrapper.get('button').attributes()).toHaveProperty('disabled');
+      expect(wrapper.findAll('.uploader__item').length).toBe(1);
+      const firstItem = wrapper.get('.uploader__item:first-child');
+
+      // 是否有正確添加 uploading class
+      expect(firstItem.classes()).toContain('uploader__item--loading');
+
       await flushPromises();
-      expect(wrapper.get('button').text()).toBe('上傳成功');
+      expect(wrapper.get('button').text()).toBe('上傳');
+      expect(firstItem.classes()).toContain('uploader__item--success');
+      expect(firstItem.get('.uploader__filename').text()).toBe(testFile.name);
+      done();
     });
   });
 
-  it('should return error text whene post is rejected', async () => {
+  it('should return error text whene post is rejected', done => {
     mockedAxios.post.mockRejectedValueOnce({ error: 'error' });
     wrapper.get('input').trigger('change');
     wrapper.vm.$nextTick(async () => {
       expect(mockedAxios.post).toHaveBeenCalledTimes(1);
-      expect(wrapper.get('button').text()).toBe('上傳中...');
+
+      // loading svg
+      expect(
+        wrapper
+          .get('button')
+          .find('svg')
+          .exists()
+      ).toBeTruthy();
+
       await flushPromises();
-      expect(wrapper.get('button').text()).toBe('上傳失敗');
+      expect(wrapper.get('button').text()).toBe('上傳');
+      expect(wrapper.findAll('.uploader__item').length).toBe(2);
+      const lastItem = wrapper.get('.uploader__item:last-child');
+      expect(lastItem.classes()).toContain('uploader__item--error');
+      await lastItem.get('.uploader__delete').trigger('click');
+      expect(wrapper.findAll('.uploader__item').length).toBe(1);
+      done();
     });
   });
 });
