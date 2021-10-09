5d1208daf2d52a81eb66163f2e6b0ef6ebdd0675
hsimao <e087754958@gmail.com>
2021-10-09 13:25:15 +0800

    feat: add get image naturalWidth and naturalHeight

--- src/Upload.ts
+++ src/Upload.ts
@@ -44,3 +44,20 @@ export const commonUploadCheck = (file: File) => {
 
   return passed;
 };
+
+// 取得圖片原始寬高
+export const getImageDimensions = (url: string) => {
+  return new Promise<{ width: number; height: number }>((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+
+    img.addEventListener('load', () => {
+      const { naturalWidth: width, naturalHeight: height } = img;
+      resolve({ width, height });
+    });
+
+    img.addEventListener('error', () => {
+      reject(new Error('There was some problem with the image.'));
+    });
+  });
+};
